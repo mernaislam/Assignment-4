6798e89a36557f2deaa56939732e3c08a68087ce
mariaehab <maria.ehab2004@gmail.com>
2022-05-12 01:04:27 +0200

    register

--- loginApp.cpp
+++ loginApp.cpp
@@ -2,9 +2,6 @@
 #include <fstream>
 #include <string>
 #include <conio.h>
-#include <unordered_map>
-#include <list>
-#include <regex>
 
 using namespace std;
 
@@ -17,9 +14,6 @@ void verifyMobile();
 void verifyEmail();
 
 fstream myFile;
-string userEmail;
-unordered_map<string, list<string>> userProfile; // store email as key and list of details as value
-list<string> userDetails {}; // store list of details {mobile , name , password}
 
 int main(){
     int choice;
@@ -45,28 +39,33 @@ int main(){
 
 void registration(){
 
-    string line;
-    int ID = 20220000;
-    myFile.open("Database.txt", ios::in);
-    while(!myFile.eof()) {
-        myFile >> line;
-        if(line.substr(line.length()-4, line.length()) == ".com"){
-            userProfile.insert(pair<string, list<string>>(line, userDetails));
-            ID++;
-        }
-    }
-    myFile.close();
+    string email, mobile, name;
     // 1. add his profile  (with personal information) to the system and select a username and a password
+    cout << "Please enter your email: " << endl;
+    cin >> email;
+    verifyEmail();
     // 2. user's email and email should not be previously registered
     // 3. Ensure email follows proper email format
-    verifyEmail();
     myFile.open("Database.txt", ios::app);
-    myFile << userEmail << endl;
+    myFile << email << endl;
     myFile.close();
-    // 4. Verify mobile format
+
+
+    cout << "Please enter your mobile number: " << endl;
+    cin >> mobile;
     verifyMobile();
-    // 5. Ensure that the name follows proper format
+    // 4. Verify mobile format
+    myFile.open("Database.txt", ios::app);
+    myFile << mobile << endl;
+    myFile.close();
+    cout << "Please enter your name :" << endl;
+    cin.ignore();
+    getline(cin, name);
     verifyName();
+    // 5. Ensure that the name follows proper format
+    myFile.open("Database.txt", ios::app);
+    myFile << name << endl;
+    myFile.close();
     // 6. Display a message to the user
     cout << "Passwords must contain:\n"
             "\n"
@@ -74,16 +73,12 @@ void registration(){
             "ْ  a minimum of 1 upper case letter [A-Z] and\n"
             "ْ  a minimum of 1 numeric character [0-9] and\n"
             "ْ  a minimum of 1 special character: ~`!@#$%^&*()-_+={}[]|\\;:\"<>,./?\n"
+            "ْ  at least 1 upper case, numeric, and special character must be EMBEDDED somewhere in the middle of the password, and not just be the first or the last character of the password string.\n"
             "ْ  Passwords must be at least 10 characters in length, but can be much longer." << endl;
 
+    cout << "Please enter your password: " << endl;
+
     verifyPassword();
-    cout << "Thank you, you have completed your registration: " << endl;
-    myFile.open("Database.txt", ios::app);
-    myFile << ID << endl << endl;
-    myFile.close();
-    userProfile.insert(pair<string, list<string>>(userEmail, userDetails));
-    cout << "Your Personal ID is: " << ID << endl;
-    main();
 }
 
 void login(){
@@ -97,8 +92,8 @@ void changePassword(){
 void verifyPassword(){
 
     string password1, password2;
+    int countUpper = 0, countLower = 0, countNum = 0, countSpecial = 0, countLetters = 0;
     int masking1, masking2;
-    cout << "Please enter your password: " << endl;
     // 9. Cover the password with ***** while the user is entering it.
     masking1 = _getch();
     while(masking1 != 13){
@@ -108,20 +103,23 @@ void verifyPassword(){
     }
     cout << endl;
     // 8. Verify that the password is a strong one.
-    string lowerCase = "(?=.*[a-z])";
-    string upperCase = "(?=.*[A-Z])";
-    string numbers = "(?=.*[0-9])";
-    string specialChar = "(?=.*[@_!#$%^&*:;\\/\"\'|.,~`<>?])";
-    string minChar = "(?=.{8,})";
-    string fullPattern2 = lowerCase + upperCase + numbers + specialChar + minChar;
-    regex regexRule2(fullPattern2);
-
-    bool isValid = regex_search(password1, regexRule2);
-    if(isValid){
-        cout << "Password is valid" << endl;
+    for(char letter : password1){
+        if(int(letter) > 96 && int(letter) < 123){
+            countLower++;
+        } else if(int(letter) > 64 && int(letter) < 91){
+            countUpper++;
+        } else if(int(letter) > 47 && int(letter) < 58){
+            countNum++;
+        } else if(int(letter) > 32){
+            countSpecial++;
+        }
+        countLetters++;
+    }
+    if(countLetters >= 10 && countSpecial >= 1 && countUpper >= 1 && countLower >= 1 && countNum >= 1){
+        cout << "Your password is strong enough! " << endl;
         // 10. If registration is valid, the user profile, ID and password are added to the list of system users and stored in the user file
         myFile.open("Database.txt", ios::app);
-        myFile << password1 << endl;
+        myFile << password1 << endl << endl;
         myFile.close();
         cout << "Please enter your password again: " << endl;
         masking2 = _getch();
@@ -136,24 +134,72 @@ void verifyPassword(){
             cout << "The two passwords are not the same, please re-enter the second password: " << endl;
             cin >> password2;
             if (password1 == password2) {
-                userDetails.push_back(password1);
+                cout << "Thank you, you have completed your registration: " << endl;
                 break;
             }
         }
     } else {
-        cout << "Password is NOT valid " << endl;
-        verifyPassword();
+        while(countLetters < 10 || countSpecial == 0 || countUpper == 0 || countLower == 0 || countNum == 0) {
+            cout << "The password is not strong enough. " << endl;
+            if(countLetters < 10){
+                cout << "You need to use more characters in your password." << endl;
+                cout << "You must use at least " << (10-countLetters) << " more characters." << endl;
+            }if(countSpecial == 0){
+                cout << "You must use at least 1 special character from: ~`!@#$%^&*()-_+={}[]|\\;:\"<>,./?" << endl;
+            }  if(countLower == 0){
+                cout << "You must use at least 1 lowercase letter: [a-z] " << endl;
+            } if(countUpper == 0){
+                cout << "You must use at least 1 uppercase letter: [A-Z] " << endl;
+            } if(countNum == 0){
+                cout << "You must use at least 1 number from [0-9] " << endl;
+            }
+            cout << "Please re-enter your password: " << endl;
+            verifyPassword();
+            break;
+        }
     }
 }
 
 void verifyName(){
 
-}
-
 void verifyMobile(){
+    string mobile;
+    cout << "Please enter your mobile number: " << endl;
+    cin >> mobile;
+
+    string regexPattern = "01[0-9]*";
+    regex regexRule(regexPattern);
+    bool isValid = regex_match(mobile, regexRule);
+    if(mobile.length() != 11 || !isValid){
+        cout << "Invalid mobile number, it must be 11 digits starts with 01: " << endl;
+        verifyMobile();
+    } else {
+        myFile.open("Database.txt", ios::app);
+        myFile << mobile << endl;
+        myFile.close();
+        userDetails.push_back(mobile);
+    }
 
 }
 
 void verifyEmail(){
-
+    string email;
+    string regexPattern = "\\w+\\.?\\w+@\\w+\\.com";
+    regex regexRule(regexPattern);
+    cout << "Please enter your email: " << endl;
+    cin >> email;
+    bool isValid = regex_match(email, regexRule);
+    if(!isValid){
+        cout << "Invalid email address format: [ohndoe@company.com] \nPlease try again! " << endl;
+        verifyEmail();
+    } else {
+        userEmail = email;
+        for(auto word : userProfile){
+            if(email == word.first){
+                cout << "This email has been registered before, please choose another one: " << endl;
+                verifyEmail();
+                break;
+            }
+        }
+    }
 }
